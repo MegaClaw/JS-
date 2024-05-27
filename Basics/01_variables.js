90c99ea4e721e86a9432f0e10bd5d558e9d955f9
Mohit Ramavtar <mohitramavtar01@gmail.com>
2024-05-27 05:01:31 +0000

    variable class done

---
+++ Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1233
+let accountEmail = "bhaiyanamaste@gmail.com"
+var accountPassword = "1234"
+accountCity = "kashmir"
+
+// accountID = 2 not allowed as const cannot be changed //
+
+console.log(accountId);
+ 
+console.log([accountEmail,accountId,accountPassword,accountCity]);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
